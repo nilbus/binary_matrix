0161fd92ebc17a1b2703ef2260f067259669208a
Matt White <mwhite@doximity.com>
2022-09-15 15:17:27 -0700

    Alpha version

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "json"
+
+PHRASE = "Hello Edward".freeze
+
+get "/", layout: :layout do
+  erb :index, locals: { binary_translation: translate_phrase, answer: PHRASE }
+end
+
+get "/answer", layout: :layout do
+  erb :answer, locals: { phrase: PHRASE }
+end
+
+def translate_phrase
+  PHRASE.split(//).map { |char| char_to_binary(char) }
+end
+
+def char_to_binary(char)
+  char.ord.to_s(2).rjust(8, "0")
+end
